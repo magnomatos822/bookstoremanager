7538c6457676bd9ecc682b3e36591e4a350ffe6a
Magno Matos <magnomatos822@gmail.com>
2021-03-17 16:50:12 -0300

    Init commit

---
+++ src/main/java/com/magnomatos/bookstoragemanager/BookstoragemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.magnomatos.bookstoragemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoragemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoragemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magnomatos/bookstoragemanager/BookstoragemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnomatos.bookstoragemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoragemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
